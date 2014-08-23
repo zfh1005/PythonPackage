0827c3a80faff080e8b693f0b383dceec08f0601
zfh1005 <zfh1005@126.com>
2014-08-23 22:49:39 +0800

    add url parse demo function
    add Wetherreport.py for demo url parse function

---
+++ Wethereeport.py
@@ -0,0 +1,19 @@
+#encoding:UTF-8
+
+import urllib.request
+
+def getHtml(url):
+    page = urllib.request.urlopen(url)
+    html = page.read()
+    page.close()
+    return html
+
+
+def getdata():
+    url="http://www.baidu.com"
+    data=urllib.request.urlopen(url).read()
+    print(data)
+
+
+if __name__ == '__main__' :
+    html = getHtml('http://www.weather.com.cn/forecast/') 
