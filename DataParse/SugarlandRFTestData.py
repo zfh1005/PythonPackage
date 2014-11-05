e55f44935d5a2ee493c355b45eb4967f4d7b8961
zfh1005 <zfh1005@126.com>
2014-11-05 08:52:43 +0800

    delete SugarlandRFTestData.py print statement
    when online used delete print statement

--- DataParse/SugarlandRFTestData.py
+++ DataParse/SugarlandRFTestData.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #!/usr/bin/env python
 import os
 import re
@@ -28,23 +30,29 @@ def parseData(sourcePath):
     value = [sourcePath]
     start = time.time()
     f = open(sourcePath, 'r')
+    
     lines = f.readlines()
     for line in lines:
         if(('PER' in line) and ("PER_" not in line) and ("_PER" not in line)):
             p = line.split(':')[-1].split('%')[0]
-            print(p)
+            #print(p)
             value.append(p)   
         if('EVM_AVG_DB' in line):
             p = line.split(':')[-1].split('dB')[0]
-            print(p)
+            #print(p)
             value.append(p)
         if('POWER_AVG_DBM' in line):
             p = line.split(':')[-1].split('dBm')[0]
-            print(p)
+            #print(p)
+            value.append(p)
+        if('Power:' in line):
+            p = line.split(':')[-1].split('\n')[0]
+            #print(p)
             value.append(p) 
     f.close
-    print(time.time() - start)
-    
+        
+    #print(time.time() - start)
+    #print(value)
     for x in value :
         if '\t' in x:
             returnResult += x
