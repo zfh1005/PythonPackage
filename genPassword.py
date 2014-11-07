343d82689168cbbc3e00573117214fba6d1f1fb6
zfh1005 <zfh1005@126.com>
2014-11-07 16:17:40 +0800

    write gen password result to result.txt
    write gen password result to result.txt

--- genPassword.py
+++ genPassword.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import string
 import random
+import time
+
+LOG_PATH = 'd:\\result.txt'
 
 '''
 Function is genPassword
@@ -10,6 +13,15 @@ param chars is the random range.Default range is lowercase letters, uppercase le
 def genPassword(length=8,chars=string.digits+string.ascii_letters):
     return ''.join(random.sample(chars*10,length))
 
+def writeLog(logPath, result):
+    f = open(logPath, 'a')
+    f.write(str(result) + '\n')
+    f.flush
+    f.close
+
 if __name__=="__main__":
+  start = time.time()
   for i in range(100000):
-    print(genPassword(16))
+    writeLog(LOG_PATH, genPassword(16))
+  print(time.time() - start, '\nFinish')  
+    
