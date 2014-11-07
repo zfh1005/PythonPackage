42baa6eec7ecdb9e7acaa548f1a88c0b837ada5b
zfh1005 <zfh1005@126.com>
2014-11-07 15:49:53 +0800

    TO Upper a list
    User isinstance() function to judge a parameter is character

--- DemoIMOOC.py
+++ DemoIMOOC.py
@@ -26,3 +26,17 @@ move(10, 'A', 'B', 'C')
 提示：range(1, 100, 2) 可以生成list [1, 3, 5, 7, 9,...]
 '''
 print ([x * ( x +1 ) for x in range(1, 100, 2)])
+
+
+'''
+请编写一个函数，它接受一个 list，然后把list中的所有字符串变成大写后返回，非字符串元素将被忽略。
+提示：
+1. isinstance(x, str) 可以判断变量 x 是否是字符串；
+2. 字符串的 upper() 方法可以返回大写的字母。
+'''
+def toUppers(L):
+    return [str.upper(x) for x in L if  isinstance(x, str)]
+
+print (toUppers(['Hello', 'world', 101]))
+
+
