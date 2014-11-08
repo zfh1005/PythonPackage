651a60279237e620b209bac64ca19e3fa6583bc8
zfh1005 <zfh1005@126.com>
2014-11-08 18:26:16 +0800

    change the output format
    change the output foramt to left alignment

--- Password/genPassword.py
+++ Password/genPassword.py
@@ -21,7 +21,15 @@ def writeLog(logPath, result):
 
 if __name__=="__main__":
   start = time.time()
-  for i in range(100000):
-    writeLog(LOG_PATH, (str(i) + '     ') + (genPassword(16)))
+  for i in range(1, 100000):
+    writeLog(LOG_PATH, (str(i).ljust(10, ' ')) + (genPassword(16)))
   print(time.time() - start, '\nFinish')  
     
+'''
+字符串在输出时的对齐：
+S.ljust(width,[fillchar])   
+#输出width个字符，S左对齐，不足部分用fillchar填充，默认的为空格。  
+S.rjust(width,[fillchar]) #右对齐  
+S.center(width, [fillchar]) #中间对齐  
+S.zfill(width) #把S变成width长，并在右对齐，不足部分用0补足   
+'''    
