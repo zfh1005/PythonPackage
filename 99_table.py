518c5904bc4e19a93cea83c72a927721c39b2138
zfh1005 <zfh1005@126.com>
2014-11-23 11:07:26 +0800

    change the output string format
    change the output string format.

--- 99_table.py
+++ 99_table.py
@@ -1,5 +1,5 @@
 for i in range(1, 10):
     for j in  range(1, 10):
         if j <= i:
-            print("%2d "%(i*j), end=" ")
-    print("\r\n")
+            print("%dx%d = %-2d "%(j, i, i*j), end=" ")
+    print("\r\n") 
