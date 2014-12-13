51ca8e90457966a2e20a1d4cb1ab22bc0fc90b00
zfh1005 <zfh1005@126.com>
2014-12-13 09:20:55 +0800

    add new file DC112AParserPowerValue
    add new file DC112AParserPowerValue.
    This file is used for parser power
    test data.

---
+++ DataParse/DC112AParserPowerValue.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+import os
+import re
+import time
+import sys
+
+TXT_FILE_EXT = ['txt', 'log']
+PARSE_PATH = 'D:\\Temp\\log'
+LOG_PATH = 'd:\\result.xls'
+
+#user os.walk list all txt/log file in forder
+#the param name is the forder name
+def listForderFileList(name):
+    start = time.time()
+    for root, dirs, files in os.walk(name):
+        for fn in files:
+            if(fn.split('.')[-1] in TXT_FILE_EXT):
+                filename = root + '\\' + fn
+                #parse data
+                result = parseData(filename)
+                writeLog(LOG_PATH, result)
+    print('finish!')
+    print(time.time() - start)
+    pass
+
+
+#parse test data
+#the param sourcePathath is filePath need parse
+def parseData(sourcePath):
+    returnResult = ''
+    value = [sourcePath.strip(), sourcePath.split('\\')[-1].split('.')[0].strip()]
+    
+    f = open(sourcePath, 'r')
+    try:
+        lines = f.readlines()
+        for line in lines:
+            if('power meter' in line):
+                p = line.strip().split('::')[-1]
+                #print(p)
+                value.append(p)
+    except:
+        pass
+    finally:    
+        f.close       
+    
+    #print(value)
+    for x in value :    
+        if '\t' in x:
+            returnResult += x
+        else:
+            returnResult += x + '\t'
+    return returnResult
+
+#write data to log file
+#param logPath is write log file path
+#param result is the data need to write
+def writeLog(logPath, result):
+    f = open(logPath, 'a')
+    f.write(str(result) + '\n')
+    f.flush
+    f.close
+
+if __name__=="__main__":   
+    if len(sys.argv) < 2:
+        listForderFileList(PARSE_PATH) 
+    else:
+        listForderFileList(sys.argv[1])     
+        
+    
