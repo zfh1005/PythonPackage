7ffc1ee801ec84a23093378686910f3e37880ece
zfh1005 <zfh1005@126.com>
2015-01-13 11:45:55 +0800

    add deleteLinkFile.py into package
    1. the package full path name is : PythonPackage\FileOperation
    2. this file is used for delete all .lnk file at all desk.

---
+++ FileOperation/deleteLinkFile.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+'''
+delete all link file at all desk
+'''
+
+import os
+import sys
+import time
+
+TXT_FILE_EXT = ['lnk']
+_PATH = []
+
+#user os.walk list all txt/log file in forder
+#the param name is the forder name
+def listForderFileList(name):
+    start = time.time()
+    for root, dirs, files in os.walk(name):
+        for fn in files:
+            #check file type is '.lnk'
+            if(fn.lower().split('.')[-1] in TXT_FILE_EXT):
+                filename = root + '\\' + fn
+                #delete '.lnk' file
+                try:
+                    print('filename ', time.time(), filename)
+                    os.remove(filename)
+                    os.unlink(filename)
+                except:
+                    pass
+                    
+    print('finish!')
+    print(time.time() - start)
+    pass
+
+#get all desk list.(Need change. Get system's desk list, not common)
+def deskList():
+    for x in string.ascii_uppercase:
+        _PATH.append(x + ':\\')
+
+if __name__=="__main__":
+    for x in _PATH:
+        listForderFileList(x)    
+    
+        
