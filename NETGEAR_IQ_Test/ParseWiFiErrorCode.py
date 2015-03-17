1eb63123f3c7533a242afc03229f93466ff9ed65
zfh1005 <zfh1005@126.com>
2015-03-17 18:06:09 +0800

    add ParseWiFiErrorCode.py into package

---
+++ NETGEAR_IQ_Test/ParseWiFiErrorCode.py
@@ -0,0 +1,79 @@
+#encoding: UTF-8
+
+ERROR_CODE_DEFINE_FILE_NAME = r'WiFiErrorCodeDefine.txt'
+ERROR_LIST = ['8.TX_VERIFY_EVM 5128 MCS7 HT20 _POWER', '-25.00', '-1.66', ' 25.00']
+ErrorCodeDict = {}
+
+def parseErrorCodeDefine():
+    f = open(ERROR_CODE_DEFINE_FILE_NAME, 'r')
+    lines = f.readlines()
+    for line in lines:
+        tempKey = line.strip().split(':')[0].strip()
+        tempValue = line.strip().split(':')[-1].strip()
+        ErrorCodeDict[tempKey] = tempValue
+    return ErrorCodeDict
+
+def getErrorCode(e):
+    assert len(e) != 0
+    errorCode = ''
+    #e = ['8.TX_VERIFY_EVM 2472 MCS7 HT20 _FREQ', '-25.00', '-1.66', ' 25.00']
+    ErrorList = e[0].split(' ')
+    #ErrorList = ['8.TX_VERIFY_EVM', '2472', 'MCS7', 'HT20', '_FREQ']
+    if '_EVM' in ErrorList[-1]:
+        errorCode += 'EV'
+        if '24' in ErrorList[1]:
+            errorCode += '04'
+        else:
+            errorCode += '14'
+    elif '_POWER' in ErrorList[-1]:
+        errorCode += 'PW'
+        if '24' in ErrorList[1]:
+            errorCode += '04'
+        else:
+            errorCode += '14'
+    elif 'FREQ' in ErrorList[-1]:
+        errorCode += 'FQ'
+        if '24' in ErrorList[1]:
+            errorCode += '04'
+        else:
+            errorCode += '14'
+    elif 'PER' in ErrorList[-1]:
+        errorCode += 'PR'
+        if '24' in ErrorList[1]:
+            errorCode += '04'
+        else:
+            errorCode += '14'
+    elif 'MASK' in ErrorList[-1]:
+        errorCode += 'MS'
+        if '24' in ErrorList[1]:
+            errorCode += '04'
+        else:
+            errorCode += '14'
+    elif '_IQ' in ErrorList[-1]:
+        errorCode += 'SY00'
+    elif '_TT00' in ErrorList[-1]:
+        errorCode += 'TT00'
+    elif '_EX01' in ErrorList[-1]:
+        errorCode += 'EX01'     
+    return  errorCode
+
+def getErrorCodeDescribe(e):
+    assert len(e) != 0
+    return ErrorCodeDict[e]
+
+def getErrorCodeAndDescribe(e):
+    assert len(e) != 0
+    parseErrorCodeDefine()
+    re = getErrorCode(e)    
+    return re, getErrorCodeDescribe(re)
+
+def getErrorInfo(e):
+    assert len(e) != 0
+     #e = ['8.TX_VERIFY_EVM 2472 MCS7 HT20 _FREQ', '-25.00', '-1.66', ' 25.00']
+    parseErrorCodeDefine()
+    re = getErrorCode(e)    
+    return [re, getErrorCodeDescribe(re), e[1]]
+    
+
+if __name__ == '__main__':
+    print(getErrorInfo(ERROR_LIST))
